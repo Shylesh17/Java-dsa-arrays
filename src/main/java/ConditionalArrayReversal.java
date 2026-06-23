3849c5db98fcf38784348fe6381032f81f7d8fa7
Shylesh17 <palshylesh00@gmail.com>
2026-06-24 00:26:24 +0530

    file renamed

--- src/main/java/ConditionalArrayReversal.java
+++ src/main/java/ConditionalArrayReversal.java
@@ -1,17 +1,19 @@
 // if seccond half of the array is greater than first then reverse the array
 
+import java.util.Arrays;
+
 public class ConditionalArrayReversal {
     public static void main(String[] args) {
         int[] arr = {1, 4, 5, 7, 9, 4, 4};
         checkarray(arr);
 
     }
-    public static int  checkarray(int[] arr){
+    public static int[]  checkarray(int[] arr){
         int firstsum=0;
         for(int i=0; i<arr.length/2;i++){
             firstsum+=arr[i];
         }
-        
+
         int secondsum=0;
         int mid = arr.length/2;
         for(int i=mid;i<arr.length;i++){
@@ -19,7 +21,18 @@ public class ConditionalArrayReversal {
         }
         System.out.println("firstsum=" + firstsum);
         System.out.println("secondsum"+ secondsum);
-        
-        return 0;
+
+        int[] reversed=new int[arr.length];
+        if(secondsum>firstsum){
+
+            for (int i=0;i<arr.length;i++){
+                reversed[i]=arr[arr.length-1-i];
+
+            }
+
+            System.out.print(Arrays.toString(reversed));
+        }
+
+        return reversed;
     }
 }
